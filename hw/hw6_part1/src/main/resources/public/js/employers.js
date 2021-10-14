ec880205862684fc3bbb04dd98b92a8926e940e4
qifanyyy <qifany4@uci.edu>
2021-10-14 15:21:15 -0400

    updated-task1

--- hw/hw6_part1/src/main/resources/public/js/employers.js
+++ hw/hw6_part1/src/main/resources/public/js/employers.js
@@ -15,17 +15,19 @@ Array.prototype.forEach.call(delButtons, function(button) {
     button.addEventListener('click', deleteEmployer.bind(null, button.id));
 });
 
-document.getElementById("add_btn").addEventListener('event', function () {
-    name = document.getElementById("name").value;
-    sector = document.getElementById("sector").value;
-    summary = document.getElementById("summary").value;
-    console.log("clicked")
-    console.log(name)
-    console.log(sector)
-    console.log(summary)
-    fetch('http://localhost:7000/employers?name=' + name + '&sector=' + sector + '&summary=' + summary, {
-        method: 'Post',
-    }).then(res => window.location.reload = window.location.reload(true));
+document.getElementById("add_btn").addEventListener('click', function () {
+    if (validateEmployee()){
+        name = document.getElementById("name").value;
+        sector = document.getElementById("sector").value;
+        summary = document.getElementById("summary").value;
+        console.log("clicked")
+        console.log(name)
+        console.log(sector)
+        console.log(summary)
+        fetch('http://localhost:7000/employers?name=' + name + '&sector=' + sector + '&summary=' + summary, {
+            method: 'Post',
+        }).then(res => window.location.reload = window.location.reload(true));
+    }
 });
 
 function validateEmployee() {
@@ -39,9 +41,8 @@ function validateEmployee() {
     if (sector.value.length < 2 || sector.value.length > 100 || sector.value.includes("$")||sector.value.includes("@")||sector.value.includes("^")||sector.value.includes("%")||sector.value.includes("~")) {
         alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~");
         return false
-    } else {
-        let event = new CustomEvent("event", {});
-        document.dispatchEvent(event);
     }
+    return true
+
 }
 
