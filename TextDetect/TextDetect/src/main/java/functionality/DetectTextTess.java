977513e24627f569455106d2148d6695029b8a8c
Yihao Liu <yliu333@jhu.edu>
2021-11-30 03:58:57 -0500

    try ocr configs

---
+++ TextDetect/TextDetect/src/main/java/functionality/DetectTextTess.java
@@ -0,0 +1,43 @@
+package functionality;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import javax.imageio.ImageIO;
+
+public class DetectTextTess {
+    public static String detect() {
+        String s = "abc";
+        Tesseract tesseract = new Tesseract();
+        try {
+
+            tesseract.setDatapath("./");
+
+            // the path of your tess data folder
+            // inside the extracted file
+            String imageURL = "https://jeroen.github.io/images/testocr.png";
+
+            URL url = new URL(imageURL);
+            BufferedImage img = ImageIO.read(url);
+            String text
+                    = tesseract.doOCR(img);
+
+            // path of your image file
+            System.out.print(text);
+            s = text;
+        } catch (TesseractException | MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+}
+
+
+
--- TextDetect/TextDetect/src/main/java/functionality/Main.java
+++ TextDetect/TextDetect/src/main/java/functionality/Main.java
@@ -1,3 +1,58 @@
+//package functionality;
+//
+//import spark.ModelAndView;
+//import spark.Spark;
+//import spark.template.velocity.VelocityTemplateEngine;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Main {
+//    static int PORT = 7000;
+//
+//    private static int getPort() {
+//        String herokuPort = System.getenv("PORT");
+//        if (herokuPort != null) {
+//            PORT = Integer.parseInt(herokuPort);
+//        }
+//        return PORT;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        Spark.port(getPort());
+////        Spark.port(PORT);
+//
+//        Spark.get("/", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            return new ModelAndView(model, "public/main.vm");
+//        }, new VelocityTemplateEngine());
+//
+//        Spark.post("/detect", (req, res) -> {
+//            String url;
+//            if (req.cookie("url") != null) {
+//                url = req.cookie("url");
+//            }
+//            else
+//            {
+//                url = req.queryParams("url");
+//            }
+//            return DetectTextTess.detect();
+////            return null;
+//        });
+//
+//
+//        Spark.post("/saveurl", (req, res) -> {
+//            String url = req.queryParams("url");
+//            res.cookie("url", url);
+//            return "";
+//        });
+//
+//    }
+//
+//}
+
+
 package functionality;
 
 import spark.ModelAndView;
@@ -8,7 +63,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    static int PORT = 7000;
+//    static int PORT = 7000;
+    static int PORT = 8081;
 
     private static int getPort() {
         String herokuPort = System.getenv("PORT");
