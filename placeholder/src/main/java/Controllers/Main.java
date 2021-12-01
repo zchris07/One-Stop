f13b17158473820379b67ffd3490fdf614bb0c2e
Yihao Liu <yliu333@jhu.edu>
2021-12-01 03:56:23 -0500

    merged imgdetect to main app

--- placeholder/src/main/java/Controllers/Main.java
+++ placeholder/src/main/java/Controllers/Main.java
@@ -4,13 +4,18 @@ import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
+
         final int PORT_NUM = 7000;
         Spark.port(PORT_NUM);
         Spark.staticFiles.location("/public");
+
+        // Init pages
         APIEndpoint.rootGet();
         APIEndpoint.signupGet();
         APIEndpoint.signupPost();
         APIEndpoint.accExist();
+
+        // User pages
         APIEndpoint.loginGet();
         APIEndpoint.loginPost();
         APIEndpoint.loginPost();
@@ -21,15 +26,26 @@ public class Main {
         APIEndpoint.nonexistPswGet();
         APIEndpoint.userprofileGet();
         APIEndpoint.userprofilePut();
+
+        // List task pages
         APIEndpoint.showlistGet();
         APIEndpoint.showDetailPut();
         APIEndpoint.addlistPost();
         APIEndpoint.deletelist();
         APIEndpoint.addtaskPost();
         APIEndpoint.deleteTask();
+
+        // Scheduling pages
         APIEndpoint.scheduleGet();
         APIEndpoint.schedulePut();
         APIEndpoint.showDetailGet();
+
+        // Image detection pages
+        APIEndpoint.imgDetectUpload();
+        APIEndpoint.imgDetectSaveUrl();
+        APIEndpoint.imgDetect();
+
+        // Main page
         APIEndpoint.main();
     }
 
